5fbc3f1c6fd00951bc825fa33095238ff255aaa9
Olulana Kolawole <kolawolegolulana@gmail.com>
2015-03-02 21:54:27 +0200

    Create XOREncryptDecrypt.java

---
+++ XOREncryptDecrypt.java
@@ -0,0 +1,24 @@
+
+public class XOREncryptDecrypt {
+
+	private static String encryptDecrypt(String input) {
+		char[] key = {'1', '4', 'A'}; 
+		StringBuilder output = new StringBuilder();
+		
+		for (int i = 0; i < input.length(); i++) {
+			output.append((char) (input.charAt(i) ^ key[i % key.length]));
+		}
+ 		
+		return output.toString();
+	}
+	
+	public static void main(String[] args) {
+		String encrypted = XOREncryptDecrypt.encryptDecrypt("google_summer_of_code");
+		System.out.println("Encrypted:" + encrypted);
+		
+		
+		String decrypted = XOREncryptDecrypt.encryptDecrypt(encrypted);
+		System.out.println("Decrypted:" + decrypted);
+	}
+
+}
